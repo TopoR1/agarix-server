0a5c710a028e47059165b728361d25c9f9f94280
TopoR <egorbelov.com@mail.ru>
2020-10-04 14:13:39 +0300

    Update MinionPlayer.js

--- src/ai/MinionPlayer.js
+++ src/ai/MinionPlayer.js
@@ -1,81 +1,83 @@
-var PlayerTracker = require('../PlayerTracker');
+const PlayerTracker = require('../PlayerTracker');
 
-function MinionPlayer() {
-    PlayerTracker.apply(this, Array.prototype.slice.call(arguments));
-    this.isMi = true; // Marks as minion
-}
-
-module.exports = MinionPlayer;
-MinionPlayer.prototype = new PlayerTracker();
-
-MinionPlayer.prototype.checkConnection = function () {
-    if (this.socket.isCloseRequest) {
-        while (this.cells.length) {
-            this.gameServer.removeNode(this.cells[0]);
-        }
-        this.isRemoved = true;
-        return;
-    }
-	this.user = {skin_used: {skin_name: ""}};
-    var skin = " "; //this.owner._skin
-	
-    if (this.owner.user_auth) {
-		if (this.owner.checkVIP() && this.owner.user.vip.botsSkins) {
-			this.minionSkins = true;
-			skin = this.owner.socket.packetHandler.getRandomSkin();
-		}
-        if (this.owner.user.clan) {
-            this.tag = this.owner.tag;
-        }
-	}
-    if (this.owner.cells.length && this.owner.minionActivity) {
-        var name = this.owner._miName;
-        try {
-            if (!name.trim()) name = this.owner._name + " Bot";
-        } catch(err) {
-            name = 'Bot';
-        }
-        this.joinGame(name, skin, true)
-        if (!this.cells.length) this.socket.close();
+class MinionPlayer {
+    constructor() {
+        PlayerTracker.apply(this, Array.prototype.slice.call(arguments));
+        this.isMi = true; // Marks as minion
     }
 
-    // remove if owner has disconnected or has no control
-    if (this.owner.socket.isConnected == false || !this.owner.minionControl)
-        this.socket.close();
+    checkConnection() {
+        if (this.socket.isCloseRequest) {
+            while (this.cells.length) {
+                this.gameServer.removeNode(this.cells[0]);
+            }
+            this.isRemoved = true;
+            return;
+        }
+        this.user = {skin_used: {skin_name: ""}};
+        let skin = " "; //this.owner._skin
+        
+        if (this.owner.user_auth) {
+            if (this.owner.checkVIP() && this.owner.user.vip.botsSkins) {
+                this.minionSkins = true;
+                skin = this.owner.socket.packetHandler.getRandomSkin();
+            }
+            if (this.owner.user.clan) {
+                this.tag = this.owner.tag;
+            }
+        }
+        if (this.owner.cells.length && this.owner.minionActivity) {
+            let name = this.owner._miName;
+            try {
+                if (!name.trim()) name = `${this.owner._name} Bot`;
+            } catch(err) {
+                name = 'Bot';
+            }
+            this.joinGame(name, skin, true)
+            if (!this.cells.length) this.socket.close();
+        }
 
-    // frozen or not
-    if (this.owner.minionFrozen) this.frozen = true;
-    else this.frozen = false;
+        // remove if owner has disconnected or has no control
+        if (this.owner.socket.isConnected == false || !this.owner.minionControl)
+            this.socket.close();
 
-    // split cells
-    if (this.owner.minionSplit)
-        this.socket.packetHandler.pressSpace = true;
+        // frozen or not
+        if (this.owner.minionFrozen) this.frozen = true;
+        else this.frozen = false;
 
-    // eject mass
-    if (this.owner.minionEject)
-        this.socket.packetHandler.pressW = true;
+        // split cells
+        if (this.owner.minionSplit)
+            this.socket.packetHandler.pressSpace = true;
 
-    // follow owners mouse by default
-    this.mouse = this.owner.mouse;
+        // eject mass
+        if (this.owner.minionEject)
+            this.socket.packetHandler.pressW = true;
 
-    // pellet-collecting mode
-    if (this.owner.collectPellets) {
-        if (this.cells.length) {
-            this.viewNodes = [];
-            var self = this;
-            this.viewBox = this.owner.viewBox;
-            this.gameServer.quadTree.find(this.viewBox, function (check) {
-                if (check.cellType == 1) self.viewNodes.push(check);
-            });
-            var bestDistance = 1e999;
-            for (const cell of this.viewNodes) {
-                var dx = this.cells[0].position.x - cell.position.x;
-                var dy = this.cells[0].position.y - cell.position.y;
-                if (dx * dx + dy * dy < bestDistance) {
-                    bestDistance = dx * dx + dy * dy;
-                    this.mouse = cell.position;
+        // follow owners mouse by default
+        this.mouse = this.owner.mouse;
+
+        // pellet-collecting mode
+        if (this.owner.collectPellets) {
+            if (this.cells.length) {
+                this.viewNodes = [];
+                const self = this;
+                this.viewBox = this.owner.viewBox;
+                this.gameServer.quadTree.find(this.viewBox, check => {
+                    if (check.cellType == 1) self.viewNodes.push(check);
+                });
+                let bestDistance = 1e999;
+                for (const cell of this.viewNodes) {
+                    const dx = this.cells[0].position.x - cell.position.x;
+                    const dy = this.cells[0].position.y - cell.position.y;
+                    if (dx * dx + dy * dy < bestDistance) {
+                        bestDistance = dx * dx + dy * dy;
+                        this.mouse = cell.position;
+                    }
                 }
             }
         }
     }
-};
+}
+
+module.exports = MinionPlayer;
+MinionPlayer.prototype = new PlayerTracker();
