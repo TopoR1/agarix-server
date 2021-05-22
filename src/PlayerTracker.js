4246ace1eebda310c3291d6b305ff98c1c1d9cbc
TopoR <egorbelov.com@mail.ru>
2021-05-22 16:46:27 +0300

    Update PlayerTracker.js

--- src/PlayerTracker.js
+++ src/PlayerTracker.js
@@ -367,6 +367,7 @@ class PlayerTracker {
         this._skin = '';
         if (((!this.isBot && !this.isMi && this.user_auth) || this.isMi && this.minionSkins) && typeof(skin) == 'string') {
             this._skin = skin;
+			console.log(skin)
         }
         const writer = new BinaryWriter();
         writer.writeStringZeroUtf8(this._skin);
