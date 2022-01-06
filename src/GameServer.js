2091cf9787824e9cc9eeaf856bca8449b3ff82a1
TopoR <egorbelov.com@mail.ru>
2022-01-06 03:42:07 +0300

    Update GameServer.js

--- src/GameServer.js
+++ src/GameServer.js
@@ -1281,7 +1281,7 @@ class GameServer {
         this.getStats();
 
         // Show stats
-        this.httpServer = https.createServer(function(req, res) {
+        this.httpServer = http.createServer(function(req, res) {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.writeHead(200);
             res.end(this.stats);
