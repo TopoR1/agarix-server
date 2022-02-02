af82897dc1801419d0e2c1232abe468b824f530e
TopoR <egorbelov.com@mail.ru>
2022-02-02 18:52:17 +0300

    Update Coin.js

--- src/entity/Coin.js
+++ src/entity/Coin.js
@@ -1,11 +1,15 @@
 var Cell = require('./Cell');
 
 function Coin() {
-    Cell.apply(this, Array.prototype.slice.call(arguments));
-    
-    this.cellType = 1;
-    this.cellOtherType = 5;
-    this.color = {r: 255, g: 215, b: 0};
+	Cell.apply(this, Array.prototype.slice.call(arguments));
+	
+	this.cellType = 1;
+	this.cellOtherType = 5;
+	this.color = {
+		r: 255,
+		g: 215,
+		b: 0
+	};
 }
 
 module.exports = Coin;
@@ -13,12 +17,12 @@ Coin.prototype = new Cell();
 
 // Main Functions
 
-Coin.prototype.onEaten = function(cell) {
+Coin.prototype.onEaten = async function(cell) {
 	if (cell.owner) {
 		if (!cell.owner.user_auth) return;
 		if (cell.owner._token && cell.owner.gameServer.db) {
 			if (cell.owner.incVirus) {
-			    await cell.owner.gameServer.db.db('agarix-db').collection('users').updateOne({
+				await cell.owner.gameServer.db.db('agarix-db').collection('users').updateOne({
 					access_token: cell.owner._token
 				}, {
 					$inc: {
@@ -26,25 +30,32 @@ Coin.prototype.onEaten = function(cell) {
 					}
 				});
 			}
-			cell.owner.gameServer.db.db('agarix-db').collection('users').updateOne({access_token: cell.owner._token}, {$inc: {coins: cell.owner.gameServer.config.coinApp, exp: cell.owner.gameServer.config.coinExp}});
+			cell.owner.gameServer.db.db('agarix-db').collection('users').updateOne({
+				access_token: cell.owner._token
+			}, {
+				$inc: {
+					coins: cell.owner.gameServer.config.coinApp,
+					exp: cell.owner.gameServer.config.coinExp
+				}
+			});
 		}
 	}
 };
 
-Coin.prototype.cantEat = function (cell) {
-    if (!cell.owner) return true;
-    if (cell.owner.user_auth) return false;
-    else return true;
+Coin.prototype.cantEat = function(cell) {
+	if (!cell.owner) return true;
+	if (cell.owner.user_auth) return false;
+	else return true;
 };
 
-Coin.prototype.onAdd = function (gameServer) {
-    gameServer.nodesCoin.push(this);
+Coin.prototype.onAdd = function(gameServer) {
+	gameServer.nodesCoin.push(this);
 };
 
-Coin.prototype.onRemove = function (gameServer) {
-    // Remove from list of foods
-    var index = gameServer.nodesCoin.indexOf(this);
-    if (index != -1) {
-        gameServer.nodesCoin.splice(index, 1);
-    }
+Coin.prototype.onRemove = function(gameServer) {
+	// Remove from list of foods
+	var index = gameServer.nodesCoin.indexOf(this);
+	if (index != -1) {
+		gameServer.nodesCoin.splice(index, 1);
+	}
 };
