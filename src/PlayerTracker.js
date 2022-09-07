4f91f193dc6ff940b1a2c95ab4411205b890a778
TopoR <egorbelov.com@mail.ru>
2022-09-08 02:37:51 +0300

    Update PlayerTracker.js

--- src/PlayerTracker.js
+++ src/PlayerTracker.js
@@ -707,7 +707,7 @@ class PlayerTracker {
         this.gameServer.ejectMass(this);
     }
     pressH() {
-        if (this.spectate || !this.gameServer.run || this.user?.role != 4) return;
+        if (this.spectate || !this.gameServer.run) return; // || this.user?.role != 4
         //this.gameServer.testMass(this);
         /*for (var i = 0; i < 100; i++) {
           this.gameServer.splitCells(this);
